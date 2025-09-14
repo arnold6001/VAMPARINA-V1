6e85478895eff4032c68c3ba0d6889d7d6bec653
Arnold Chirchir <arnoldkipruto193@gmail.com>
2025-09-14 17:42:03 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight Bot',
-  author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  packname: 'VAMPARINA V1',
+  author: 'ARNOLD CHIRCHIR‎',
+  botName: "VAMPARINA V1",
+  botOwner: 'ARNOLD CHIRCHIR', // Your name
+  ownerNumber: '254703110780', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
