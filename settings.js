6a7058a526b5d88cae7246d23cc165c98b8ca0f2
Arnold Chirchir <arnoldkipruto193@gmail.com>
2025-10-07 16:14:14 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,7 +10,7 @@ const settings = {
   storeWriteInterval: 10000,
   description: "This is a bot for managing group commands and automating tasks.",
   version: "2.2.0",
-  updateZipUrl: "https://github.com/mruniquehacker/Knightbot-MD/archive/refs/heads/main.zip",
+  updateZipUrl: "https://github.com/arnold6001/VAMPARINA-V1/archive/refs/heads/main.zip",
 };
 
 module.exports = settings;
